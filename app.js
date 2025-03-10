91c0f86b38856c34baf6a58a34186bdfc363b891
Salma Lovyanti <salmalovyanti@gmail.com>
2025-03-10 22:44:43 +0700

    Update app.js

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ const ticketsRoutes = require('./routes/ticketsRoutes');
 const ordersRoutes = require('./routes/ordersRoutes');
 const eventsRoutes = require('./routes/eventsRoutes');
 const order_itemsRoutes = require('./routes/order_itemsRoutes');
+const paymentsRoutes = require('./routes/paymentsRoutes');
+const usersRoutes = require('./routes/usersRoutes');
+const waitingQueueRoutes = require('./routes/waitingQueueRoutes');
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -17,6 +20,9 @@ app.use(ticketsRoutes);
 app.use(ordersRoutes);
 app.use(eventsRoutes);
 app.use(order_itemsRoutes);
+app.use(paymentsRoutes);
+app.use(usersRoutes);
+app.use(waitingQueueRoutes);
 
 // Menjalankan server
 app.listen(port, () => {
