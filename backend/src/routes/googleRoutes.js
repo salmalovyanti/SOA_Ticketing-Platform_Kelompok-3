fab54c984009dd459394335fe12ddf56055b335a
Dini Rahmawati <119056585+dinrap@users.noreply.github.com>
2025-04-18 21:32:44 +0700

    Update googleRoutes.js

--- backend/src/routes/googleRoutes.js
+++ backend/src/routes/googleRoutes.js
@@ -1,12 +1,10 @@
 const express = require('express');
 const router = express.Router();
 
-// Pastikan controller ini sesuai dengan struktur folder dan sudah benar
 const { sendMail } = require('../controllers/gmailController');
 const { createEvent } = require('../controllers/calendarController');
 const { getPlaceInfo } = require('../controllers/mapsController');
 
-// Pastikan masing-masing fungsi controller terdefinisi dengan benar
 router.post('/gmail/send', sendMail);
 router.post('/calendar/create', createEvent);
 router.get('/maps/search', getPlaceInfo);
