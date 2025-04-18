8519e61b4f1cd018f33c4d84dc77438684dae4f6
Salma Lovyanti <salmalovyanti@gmail.com>
2025-04-19 02:03:30 +0700

    feat: add promo_code.model.js

---
+++ backend/src/pages/promo_code/promo_code.model.js
@@ -0,0 +1,56 @@
+const { DataTypes } = require('sequelize');
+const sequelize = require('../../config/database');
+
+const PromoCode = sequelize.define('PromoCode', {
+  promo_codes_id: {
+    type: DataTypes.BIGINT,
+    primaryKey: true,
+    allowNull: false,
+    autoIncrement: true
+  },
+  code: {
+    type: DataTypes.STRING(50),
+    allowNull: false
+  },
+  discount_percentage: {
+    type: DataTypes.INTEGER,
+    allowNull: false
+  },
+  max_discount: {
+    type: DataTypes.DECIMAL(12, 2),
+    allowNull: false
+  },
+  start_date: {
+    type: DataTypes.DATE,
+    allowNull: false
+  },
+  end_date: {
+    type: DataTypes.DATE,
+    allowNull: false
+  },
+  quota: {
+    type: DataTypes.INTEGER,
+    allowNull: false
+  },
+  created_at: {
+    type: DataTypes.DATE,
+    allowNull: false,
+    defaultValue: sequelize.literal('CURRENT_TIMESTAMP')
+  },
+  updated_at: {
+    type: DataTypes.DATE,
+    allowNull: false,
+    defaultValue: sequelize.literal('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP')
+  },
+  deleted_at: {
+    type: DataTypes.DATE,
+    allowNull: true
+  }
+}, {
+  tableName: 'promo_codes',
+  timestamps: false,
+  paranoid: true,
+  underscored: true
+});
+
+module.exports = PromoCode;
