12d9cde51ba2ed6f4292b4e445d592c50fc5c2c8
Salma Lovyanti <salmalovyanti@gmail.com>
2025-04-18 22:53:30 +0700

    update user.service.js and add avatar directory

--- backend/src/pages/user/user.service.js
+++ backend/src/pages/user/user.service.js
@@ -1,5 +1,5 @@
-const db = require('../../config/database'); 
-const { User } = require('../../models'); 
+const db = require('../../config/database');
+const { User } = require('../../models');
 
 exports.create = async (data) => {
   return await User.create(data);
@@ -28,16 +28,23 @@ exports.delete = async (id) => {
 };
 
 exports.updateProfile = async (id, data) => {
-    const user = await User.findByPk(id);
-    if (!user) return null;
-  
-    // Hanya update field tertentu untuk profile
-    const fieldsToUpdate = {};
-    if (data.name !== undefined) fieldsToUpdate.name = data.name;
-    if (data.email !== undefined) fieldsToUpdate.email = data.email;
-    if (data.phone !== undefined) fieldsToUpdate.phone = data.phone;
-  
-    await user.update(fieldsToUpdate);
-    return user;
-  };
-  
+  const user = await User.findByPk(id);
+  if (!user) return null;
+
+  // Hanya update field tertentu untuk profile
+  const fieldsToUpdate = {};
+  if (data.name !== undefined) fieldsToUpdate.name = data.name;
+  if (data.email !== undefined) fieldsToUpdate.email = data.email;
+  if (data.phone !== undefined) fieldsToUpdate.phone = data.phone;
+
+  await user.update(fieldsToUpdate);
+  return user;
+};
+
+exports.updateAvatar = async (id, avatarUrl) => {
+  const user = await User.findByPk(id);
+  if (!user) return null;
+
+  await user.update({ avatar_url: avatarUrl });
+  return user;
+};
