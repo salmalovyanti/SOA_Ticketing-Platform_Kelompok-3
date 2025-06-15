38ebe1d4e75c8689766de52fb9fc54ab0da3230d
Salma Lovyanti <salmalovyanti@gmail.com>
2025-06-15 21:53:06 +0700

    feat: integrate JWT verification on category routes

--- backend/src/pages/category/category.routes.js
+++ backend/src/pages/category/category.routes.js
@@ -1,16 +1,17 @@
 const express = require('express');
 const router = express.Router();
 const controller = require('./category.controller');
+const { authenticateToken } = require('../../middleware/authMiddleware');
 
 // Endpoint untuk menampilkan keseluruhan kategori event
 router.get('/', controller.getAllCategories);
 // Endpoint untuk menambahkan kategori event
-router.post('/', controller.createCategory);
+router.post('/', authenticateToken, controller.createCategory);
 // Endpoint untuk melihat satu kategori event
 router.get('/:id', controller.getCategoryById);
 // Endpoint untuk mengedit kategori event
-router.put('/:id', controller.updateCategory);
+router.put('/:id', authenticateToken, controller.updateCategory);
 // Endpoint untuk menghapus satu kategori event
-router.delete('/:id', controller.deleteCategory);
+router.delete('/:id', authenticateToken, controller.deleteCategory);
 
 module.exports = router;
